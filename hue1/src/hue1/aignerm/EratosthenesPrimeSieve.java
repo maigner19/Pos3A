737ded83c7df02420dcdbf31ceb0bdca53784d77
AignerM190024 <aignerm190024@sus.htl-grieskirchen.at>
2021-09-21 14:33:25 +0200

    second commit

---
+++ hue1/src/hue1/aignerm/EratosthenesPrimeSieve.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hue1.aignerm;
+
+/**
+ *
+ * @author Michael
+ */
+public class EratosthenesPrimeSieve implements PrimeSieve{
+    final int OBERGRENZE;
+
+    public EratosthenesPrimeSieve(int obergrenze) {
+        this.OBERGRENZE = obergrenze;
+    }
+    
+    
+    @Override
+    public boolean isPrime(int p) {
+        
+        
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
+        if(OBERGRENZE >= 0){
+            
+        }
+    }
+    
+}
---
+++ hue1/src/hue1/aignerm/PrimeSieve.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hue1.aignerm;
+
+/**
+ *
+ * @author Michael
+ */
+public interface PrimeSieve {
+    public boolean isPrime(int p) ;
+    public void printPrimes();
+}
